6e7adc8cb5f8ef0d1a9d68104d039631a3c9a938
DESKTOP-V0TP74J\user <tnstjddlrl@naver.com>
2020-12-29 15:17:19 +0900

    fb에 로그인 및 회원가입 가능. 사용자 온라인상태 추적 가능

--- App.js
+++ App.js
@@ -2,9 +2,12 @@ import React,{useEffect} from 'react';
 import { View,Text,Alert } from 'react-native';
 import database from '@react-native-firebase/database';
 import messaging from '@react-native-firebase/messaging';
+import auth from '@react-native-firebase/auth';
 
 const App = ()=>{
   test()
+  signin('tnstjd@naver.com','zxcv0903')
+  alive()
   return (
     <View>
       <Text>님 안녕하세요</Text>
@@ -20,6 +23,50 @@ function test() {
     });
 
     return unsubscribe;
+  }, []);
+}
+function signup(email,pass){
+  auth()
+  .createUserWithEmailAndPassword(email, pass)
+  .then(() => {
+    console.log('User account created & signed in!');
+  })
+  .catch(error => {
+    if (error.code === 'auth/email-already-in-use') {
+      console.log('That email address is already in use!');
+    }
+
+    if (error.code === 'auth/invalid-email') {
+      console.log('That email address is invalid!');
+    }
+
+    console.error(error);
+  });
+}
+
+function signin(email,pass){
+  auth()
+  .signInWithEmailAndPassword(email, pass)
+  .then(() => {
+    console.log('User account created & signed in!');
+  })
+}
+
+function alive() {
+  useEffect(() => {
+    // Assuming user is logged in
+    const userId = auth().currentUser.uid;
+
+    const reference = database().ref(`/online/${userId}`);
+
+    // Set the /users/:userId value to true
+    reference.set(true).then(() => console.log('Online presence set'));
+
+    // Remove the node whenever the client disconnects
+    reference
+      .onDisconnect()
+      .remove()
+      .then(() => console.log('On disconnect function configured.'));
   }, []);
 }
 
